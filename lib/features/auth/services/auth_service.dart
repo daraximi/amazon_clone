64a608bebb06666dac89b1004d06783df0fd42bc
Oloyede Oluwadarasimi <darasimioloyede@gmail.com>
2024-01-25 14:40:05 +0000

    Added provider and shared preferences for persistent storage

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -5,9 +5,13 @@ import 'dart:convert';
 import 'package:amazon_clone/constants/error_handling.dart';
 import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:amazon_clone/constants/utils.dart';
+import 'package:amazon_clone/features/home/screens/home_screen.dart';
 import 'package:amazon_clone/models/user_model.dart';
+import 'package:amazon_clone/providers/user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
   //Sign Up User
@@ -63,8 +67,13 @@ class AuthService {
       httpErrorHandle(
           response: response,
           context: context,
-          onSuccess: () {
-            showSnackBar(context, 'Logged in successfully');
+          onSuccess: () async {
+            SharedPreferences prefs = await SharedPreferences.getInstance();
+            Provider.of<UserProvider>(context, listen: false)
+                .setUser(response.body);
+            prefs.setString('x-auth-token', jsonDecode(response.body)['token']);
+            Navigator.pushNamedAndRemoveUntil(
+                context, HomeScreen.routeName, (route) => false);
           });
     } catch (e) {
       showSnackBar(context, e.toString());
